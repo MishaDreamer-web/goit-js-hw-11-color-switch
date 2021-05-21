0e6d0ea796b5df723f6f192de4e2749e6a6b5c0f
MishaDreamer-web <shondayn@gmail.com>
2021-05-21 18:03:14 +0300

    Добаляет файлы по Дз11

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const TIMEDELAY = 1000;
+let colorInterval = null;
+
+const startBtn = document.querySelector('[data-action="start"]');
+// console.log(startBtn);
+
+const stopBtn = document.querySelector('[data-action="stop"]');
+// console.log(stopBtn);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  console.log('click on start');
+  colorInterval = setInterval(() => {
+    document.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, TIMEDELAY);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStopBtnClick() {
+  console.log('click on stop');
+  clearInterval(colorInterval);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+// document.body.style.backgroundColor = colors[2];
